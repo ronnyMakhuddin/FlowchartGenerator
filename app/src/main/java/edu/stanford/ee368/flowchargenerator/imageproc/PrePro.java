e76a3fbbfe9ddebd2c9958055cb1d65997e529c7
zhangrao1990 <zhangrao1990@gmail.com>
2018-03-13 16:42:45 -0700

    add diamond detection
    the most important parameter is ‘smallArea’, which determines whether
    we detect the arrows or not

--- app/src/main/java/edu/stanford/ee368/flowchargenerator/imageproc/PrePro.java
+++ app/src/main/java/edu/stanford/ee368/flowchargenerator/imageproc/PrePro.java
@@ -1,4 +1,5 @@
-package edu.stanford.ee368.flowchargenerator.imageproc;
+//package edu.stanford.ee368.flowchargenerator.imageproc;
+package opencv;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -8,11 +9,20 @@ import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 public class PrePro {
+	/**
+	 * 
+	 * @param mat: input image
+	 * @return Mat[]:
+	 * Mat[0] = rectangles
+	 * Mat[1] = diamonds
+	 * Mat[2] = arrows
+	 */
 
 	public static Mat[] prepro(Mat mat){
 		try {
@@ -36,18 +46,9 @@ public class PrePro {
 			Core.bitwise_not(bina, bina); 
 			System.out.println("BitWiseNot Done!");
 
-			// remove small regions
-			int smallArea = 100;
-			List<MatOfPoint> noiseContours = new ArrayList<>();
-			Mat denoise = Mat.zeros(rows, cols, CvType.CV_8UC1);
-			Scalar white = new Scalar(255);
-			Imgproc.findContours(bina, noiseContours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-			for (MatOfPoint contour: noiseContours) {
-				if(contour.rows() > smallArea) {
-					Imgproc.fillPoly(denoise, Arrays.asList(contour), white);
-				}
-			}
-			System.out.println("Denoise Done!");
+			// denoise and fill
+			int threshold = 100;
+			Mat denoise = denoiseAndFill(bina, threshold);
 
 			// get edges
 			Mat edges = new Mat(rows, cols, CvType.CV_8UC1);
@@ -57,55 +58,13 @@ public class PrePro {
 			// hough transform
 			Mat substitute = new Mat();
 			Mat lines = new Mat(rows, cols, CvType.CV_8UC1);
-			Imgproc.HoughLinesP(edges, lines, 1, Math.PI / 180.0, 100, 100, 5);
+//			Imgproc.HoughLinesP(edges, lines, 1, Math.PI / 180.0, 100, 100, 5);
+			Imgproc.HoughLinesP(edges, lines, 1, Math.PI / 180.0, 50, 30, 5);
+
+			// rotate
 			if(lines.rows() > 0) {
-				int[] nums = new int[lines.rows() * 4];
-				double[] angles = new double[lines.rows()];
-				lines.get(0, 0, nums);
-				for(int i = 0; i < lines.rows() ; i++) {
-					double x1 = nums[4 * i];
-					double y1 = nums[4 * i + 1];
-					double x2 = nums[4 * i + 2];
-					double y2 = nums[4 * i + 3];
-					angles[i] = Math.atan((y2 - y1) / (x2 - x1));
-				}
-				Arrays.sort(angles);
-				double angle = angles[angles.length / 2];
-				double degree = Math.toDegrees(angle);
-				System.out.println("Hough Transform Done!");
-
-				// image rotate
-				int x = cols;
-				int y = rows;
-				// 4 points, (0, 0) (x, 0) (0, y) (x, y) to rotate transform (clockwise)
-				// (0, 0), (x*cosA, -x*sinA) (y*sinA, y*cosA) (x*cosA+y*sinA, -x*sinA+y*cosA)
-				int newWidth = 0;
-				int newHeight = 0;
-				int xshift = 0;
-				int yshift = 0;
-				if(angle >= 0) {
-					newWidth = (int) Math.round(x * Math.cos(angle) + y * Math.sin(angle));
-					newHeight = (int) Math.round(y * Math.cos(angle) + x * Math.sin(angle));
-					yshift = (int) Math.round(x * Math.sin(angle));
-				} else { // angle < 0
-					newWidth = (int) Math.round(x * Math.cos(angle) - y * Math.sin(angle));
-					newHeight = (int) Math.round(-x * Math.sin(angle) + y * Math.cos(angle));
-					xshift = (int) Math.round(-y * Math.sin(angle));
-				}
-
-				// shift image
-				Mat shifted = Mat.zeros(y + yshift, x + xshift, CvType.CV_8UC1);
-				Mat shiftedSub = shifted.submat(yshift, y + yshift, xshift, x + xshift);
-				denoise.copyTo(shiftedSub);
-				System.out.println("Shift Done!");
-
-				// rotate image
-				Mat rotMat = Imgproc.getRotationMatrix2D(new org.opencv.core.Point(xshift, yshift), degree, 1.0);
-				Mat rotated = new Mat();
-				Imgproc.warpAffine(shifted, rotated, rotMat, new Size(newWidth, newHeight), Imgproc.INTER_LINEAR);			
-				System.out.println("Rotation Done!");
-
-				// parameter replace
+			    Mat rotated = rotate(denoise, lines);
+				// parameter update
 				cols = rotated.cols();
 				rows = rotated.rows();
 				substitute = rotated.clone();
@@ -113,43 +72,26 @@ public class PrePro {
 				substitute = denoise.clone();
 			}
 
-			// Find contours
-			smallArea = 100;
-			List<MatOfPoint> contours = new ArrayList<>();
-			Mat fill = Mat.zeros(rows, cols, CvType.CV_8UC1);
-			Imgproc.findContours(substitute, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+			// find contours
+			int smallRegion = 100;
+			Mat fill = denoiseAndFill(substitute, smallRegion);
 
-			// Draw contours in fill Mat
-			for (MatOfPoint contour: contours) {
-				if(contour.rows() > smallArea) {
-					Imgproc.fillPoly(fill, Arrays.asList(contour), white);
-				}
-			}
-			System.out.println("Fill Done!");
-
-			// open
+			// open to eliminate arrows
 			int radius = 10;
 			Mat seOpen = seGen(radius);
 			Mat opened = new Mat(rows, cols, CvType.CV_8UC1);
 			Imgproc.morphologyEx(fill, opened, Imgproc.MORPH_OPEN, seOpen);
 			System.out.println("Open Done!");
 
-			// compute difference
+			// compute difference to get arrows
 			Mat diff = new Mat(rows, cols, CvType.CV_8UC1);
 			Core.absdiff(fill, opened, diff);
 			System.out.println("Diff Done!");
 
-			// remove small area
-			smallArea = 85;
-			List<MatOfPoint> smallContours = new ArrayList<>();
-			Mat remv = Mat.zeros(diff.size(), CvType.CV_8UC1);
-			Imgproc.findContours(diff, smallContours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
-			for (MatOfPoint contour: smallContours) {
-				if(contour.rows() > smallArea) {
-					Imgproc.fillPoly(remv, Arrays.asList(contour), white);
-				}
-			}
-			System.out.println("SmallAreaRemoval Done for Arrow!");
+			// remove small area generated by open
+			int smallArea = 50;
+//			int smallArea = 150;
+			Mat remv = denoiseAndFill(diff, smallArea);
 
 			// get rectangles and diamonds
 			Mat blob = new Mat(rows, cols, CvType.CV_8UC1);
@@ -157,43 +99,133 @@ public class PrePro {
 			System.out.println("Blob Done!");
 
 			// distinguish rectangles and diamonds
+			Mat[] rectAndDiam = genRectAndDiam(blob);
+			Mat rectangle = rectAndDiam[0];
+			Mat diamond = rectAndDiam[1];
+
 			// erode
-			int r = 10;
-			Mat seErode = seGen(r);
-			Mat eroded = new Mat(rows, cols, CvType.CV_8UC1);
-			Imgproc.erode(blob, eroded, seErode);
-			System.out.println("Erode Done!");
+			Mat eroded_rect = erode(rectangle, 10);
+			Mat eroded_diam = erode(diamond, 10);
 
 			// get boxes
-			Mat box = new Mat(rows, cols, CvType.CV_8UC1);
-			Core.absdiff(blob, eroded, box);
+			Mat rect = new Mat(rows, cols, CvType.CV_8UC1);
+			Core.absdiff(rectangle, eroded_rect, rect);
+			Mat diam = new Mat(rows, cols, CvType.CV_8UC1);
+			Core.absdiff(diamond, eroded_diam, diam);
 			System.out.println("Box Done!");
 
-			// remove small area
-
 			// type conversion
-			int[][] ret = new int[2][rows * cols];
-			Mat[] result = new Mat[2];
-			result[0] = box;
-			result[1] = remv;
+			Mat[] result = new Mat[3];
+			result[0] = rect;
+			result[1] = diam;
+			result[2] = remv;
 			System.out.println("Conversion Done!");
-
-			// convert mat2 to image
-			Mat mat2 = box;
-			byte[] data2 = new byte[mat2.rows() * mat2.cols() * (int)(mat2.elemSize())];
-			mat2.get(0, 0, data2);
 
 			return result;
 
 		} catch (Exception e) {
 			System.out.println("Error: " + e.getMessage());
 		}
-
 		return null;
+	}	
+	
+	// rotate
+	public static Mat rotate(Mat m, Mat houghLines) {
+		int[] nums = new int[houghLines.rows() * 4];
+		double[] angles = new double[houghLines.rows()];
+		houghLines.get(0, 0, nums);
+		for(int i = 0; i < houghLines.rows() ; i++) {
+			double x1 = nums[4 * i];
+			double y1 = nums[4 * i + 1];
+			double x2 = nums[4 * i + 2];
+			double y2 = nums[4 * i + 3];
+			angles[i] = Math.atan((y2 - y1) / (x2 - x1));
+		}
+		Arrays.sort(angles);
+		double angle = angles[angles.length / 2];
+		double degree = Math.toDegrees(angle);
+		System.out.println("Hough Transform Done!");
 
+		// image rotate
+		int x = m.cols();
+		int y = m.rows();
+		// 4 points, (0, 0) (x, 0) (0, y) (x, y) to rotate transform (clockwise)
+		// (0, 0), (x*cosA, -x*sinA) (y*sinA, y*cosA) (x*cosA+y*sinA, -x*sinA+y*cosA)
+		int newWidth = 0;
+		int newHeight = 0;
+		int xshift = 0;
+		int yshift = 0;
+		if(angle >= 0) {
+			newWidth = (int) Math.round(x * Math.cos(angle) + y * Math.sin(angle));
+			newHeight = (int) Math.round(y * Math.cos(angle) + x * Math.sin(angle));
+			yshift = (int) Math.round(x * Math.sin(angle));
+		} else { // angle < 0
+			newWidth = (int) Math.round(x * Math.cos(angle) - y * Math.sin(angle));
+			newHeight = (int) Math.round(-x * Math.sin(angle) + y * Math.cos(angle));
+			xshift = (int) Math.round(-y * Math.sin(angle));
+		}
 
+		// shift image
+		Mat shifted = Mat.zeros(y + yshift, x + xshift, CvType.CV_8UC1);
+		Mat shiftedSub = shifted.submat(yshift, y + yshift, xshift, x + xshift);
+		m.copyTo(shiftedSub);
+		System.out.println("Shift Done!");
+
+		// rotate image
+		Mat rotMat = Imgproc.getRotationMatrix2D(new org.opencv.core.Point(xshift, yshift), degree, 1.0);
+		Mat rotated = new Mat();
+		Imgproc.warpAffine(shifted, rotated, rotMat, new Size(newWidth, newHeight), Imgproc.INTER_LINEAR);			
+		System.out.println("Rotation Done!");
+		
+		return rotated;
+	}
+	
+	// distinguish rectangle and diamond
+	public static Mat[] genRectAndDiam(Mat m) {
+		List<MatOfPoint> blobContours = new ArrayList<>();
+		Mat rectangle = Mat.zeros(m.size(), CvType.CV_8UC1);
+		Mat diamond = Mat.zeros(m.size(),  CvType.CV_8UC1);
+		Scalar white = new Scalar(255);
+		Imgproc.findContours(m, blobContours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+		for (MatOfPoint contour: blobContours) {
+			 double actualArea = Imgproc.contourArea(contour);
+			 Rect bounding = Imgproc.boundingRect(contour);
+			 double boundingArea = bounding.width * bounding.height;
+			 if(actualArea / boundingArea > 0.75) {	// rectangular
+				 Imgproc.fillPoly(rectangle, Arrays.asList(contour), white);
+			 } else {	// diamond
+				 Imgproc.fillPoly(diamond, Arrays.asList(contour), white);
+			 }
+		}
+		System.out.println("Rectangle Diamond Done!");
+		return new Mat[]{rectangle, diamond};
+	}
+	
+	// remove small area
+	public static Mat denoiseAndFill(Mat m, int thres) {
+		List<MatOfPoint> noiseContours = new ArrayList<>();
+		Mat denoise = Mat.zeros(m.rows(), m.cols(), CvType.CV_8UC1);
+		Scalar white = new Scalar(255);
+		Imgproc.findContours(m, noiseContours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+		for (MatOfPoint contour: noiseContours) {
+			if(contour.rows() > thres) {
+				Imgproc.fillPoly(denoise, Arrays.asList(contour), white);
+			}
+		}
+		System.out.println("Denoise Done!");
+		return denoise;
+	}
+	
+	// erode
+	public static Mat erode(Mat m, int r) {
+		Mat seErode = seGen(r);
+		Mat eroded = new Mat(m.rows(), m.cols(), CvType.CV_8UC1);
+		Imgproc.erode(m, eroded, seErode);
+		System.out.println("Erode Done!");
+		return eroded;
 	}
 
+	// 2D square to 1D array
 	public static int[] TwoDim2OneDim(int[][] input, int size) {
 		int[] output = new int[size * size];
 		for(int r = 0; r < size; r++) {
@@ -204,6 +236,7 @@ public class PrePro {
 		return output;
 	}
 
+	// structuring element generation
 	public static Mat seGen(int radius) {
 		int diameter = 2 * radius + 1;
 		int[][] disk = new int[diameter][diameter];
